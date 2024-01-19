c8301119424610fe2fe8952ae0a02a8e6d345eac
unknown <sanusingh1603@gmail.com>
2024-01-19 15:42:09 +0530

    Add @radix-ui/react-tabs package and implement joined date functionality

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,108 @@
+import { Button } from "@/components/ui/button";
+import { getUserInfo } from "@/lib/actions/user.action";
+import { URLProps } from "@/types";
+import { SignedIn, auth } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getJoinedDate } from "@/lib/utils";
+import ProfileLink from "@/components/shared/ProfileLink";
+import Stats from "@/components/shared/Stats";
+import QuestionsTab from "@/components/shared/QuestionsTab";
+import AnswersTab from "@/components/shared/AnswersTab";
+
+const Profile = async ({ params, searchParams }: URLProps) => {
+  const userInfo = await getUserInfo({ userId: params.id });
+  const { userId: clerkId } = auth();
+  console.log(userInfo);
+  return (
+    <>
+      <div className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 sm:flex-row">
+          <Image
+            src={userInfo?.user?.picture}
+            alt="profile"
+            width={140}
+            height={140}
+            className="rounded-full object-cover"
+          />
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">
+              {userInfo.user.name}
+            </h2>
+            <p className="paragraph-regular text-dark200_light800">
+              @{userInfo.user.username}
+            </p>
+            <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+              {userInfo.user.portfolioWebsite && (
+                <ProfileLink
+                  imgUrl="/assets/icons/link.svg"
+                  href={userInfo.user.portfolioWebsite}
+                  title="Portfolio"
+                />
+              )}
+              {userInfo.user.location && (
+                <ProfileLink
+                  imgUrl="/assets/icons/location.svg"
+                  title={userInfo.user.location}
+                />
+              )}
+              <ProfileLink
+                imgUrl="/assets/icons/calendar.svg"
+                title={getJoinedDate(userInfo.user.joinedAt)}
+              />
+            </div>
+            {userInfo.user.bio && (
+              <p className="paragraph-regular text-dark300_light900">
+                {userInfo.user.bio}
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-5">
+          <SignedIn>
+            {clerkId === userInfo.user.clerkId && (
+              <Link href={"/profile/edit"}>
+                <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-[46px] min-w-[175px] px-4 py-3">
+                  Edit Profile
+                </Button>
+              </Link>
+            )}
+          </SignedIn>
+        </div>
+      </div>
+      <Stats
+        totalQuestions={userInfo.totalQuestions}
+        totalAnswers={userInfo.totalAnswers}
+      />
+      <div className="mt-10 flex gap-10">
+        <Tabs defaultValue="top-posts" className="flex-1">
+          <TabsList className="background-light800_dark300 min-h-[42px] p-1">
+            <TabsTrigger value="top-posts" className="tab">
+              Top Posts
+            </TabsTrigger>
+            <TabsTrigger value="answers" className="tab">
+              Answers
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="top-posts">
+            <QuestionsTab
+              searchParams={searchParams}
+              userId={userInfo.user.id}
+              clerkId={clerkId}
+            />
+          </TabsContent>
+          <TabsContent value="answers" className="flex w-full flex-col gap-6">
+            <AnswersTab
+              searchParams={searchParams}
+              userId={userInfo.user.id}
+              clerkId={clerkId}
+            />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
