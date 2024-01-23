aecb51652f3e6fb1791600dae0ebe9e7aae783a5
unknown <sanusingh1603@gmail.com>
2024-01-23 21:59:16 +0530

    Added Pagination and Filters in all the pages

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -8,6 +8,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { formUrlQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface FilterProps {
   filters: { name: string; value: string }[];
@@ -19,12 +21,26 @@ const Filter = ({
   otherClasses = "",
   containerClasses = "",
 }: FilterProps) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const paramFilter = searchParams.get("filter") || "";
+  const handleUpdateParams = (value: string) => {
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: "filter",
+      value,
+    });
+    router.push(newUrl, { scroll: false });
+  };
   return (
     <div className={`relative ${containerClasses}`}>
       {/* {filters.map((filter: any) => (
         <FilterItem key={filter.id} filter={filter} />
       ))} */}
-      <Select>
+      <Select
+        onValueChange={handleUpdateParams}
+        defaultValue={paramFilter || undefined}
+      >
         <SelectTrigger
           className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
         >
