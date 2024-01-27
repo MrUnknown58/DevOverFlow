e6068466f12a4b250d3452b2d315a9585c13d4a6
unknown <sanusingh1603@gmail.com>
2024-01-27 20:22:49 +0530

    added speed insights

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -19,6 +20,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       {/* <Toaster /> */}
       <Toaster richColors />
       <Analytics />
+      <SpeedInsights />
     </main>
   );
 };
